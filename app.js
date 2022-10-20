fc28f9c819818badde8564d7080edd2bc33ca750
Maria Sorokina <113168296+bijou743@users.noreply.github.com>
2022-10-20 13:03:03 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,97 @@
+const cols = document.querySelectorAll('.col');
+
+document.addEventListener('keydown', (event) => {
+	event.preventDefault();
+
+	if (event.code.toLowerCase() === 'space') {
+		setRandomColors();
+	}
+});
+
+document.addEventListener('click', (event) => {
+	const type = event.target.dataset.type;
+
+	if (type === 'lock') {
+		const node =
+			event.target.tagName.toLowerCase() === 'i'
+				? event.target
+				: event.target.children[0];
+
+		node.classList.toggle('fa-lock-open');
+		node.classList.toggle('fa-lock');
+	} else if (type === 'copy') {
+		copyToClipboard(event.target.textContent);
+	}
+});
+
+function setRandomColors(isInitial) {
+	const colors = isInitial ? getColorsFromHash() : [];
+
+	cols.forEach((col, index) => {
+		const isLocked = col.querySelector('i').classList.contains('fa-lock');
+		const text = col.querySelector('h2');
+		const button = col.querySelector('button');
+
+		if (isLocked) {
+			colors.push(text.textContent);
+			return;
+		}
+
+		const color = isInitial
+			? colors[index]
+				? colors[index]
+				: chroma.random()
+			: chroma.random();
+
+		text.innerHTML = color;
+		setTextColor(text, color);
+
+		setTextColor(button, color);
+
+		col.style.backgroundColor = color;
+
+		if (!isInitial) {
+			colors.push(color);
+		}
+	});
+
+	updateColorsHash(colors);
+}
+
+function setTextColor(text, color) {
+	const luminance = chroma(color).luminance();
+	text.style.color = luminance > 0.5 ? 'black' : 'white';
+}
+
+function copyToClipboard(text) {
+	return navigator.clipboard.writeText(text);
+}
+
+function updateColorsHash(colors = []) {
+	document.location.hash = colors
+		.map((color) => color.toString().substring(1))
+		.join('-');
+}
+
+function getColorsFromHash() {
+	if (document.location.hash.length > 1) {
+		return document.location.hash
+			.substring(1)
+			.split('-')
+			.map((color) => `#${color}`);
+	}
+	return [];
+}
+
+setRandomColors(true);
+
+// function generateRandomColor() {
+// 	const hexCodes = '0123456789ABCDEF';
+
+// 	let color = '';
+// 	for (let i = 0; i < 6; i++) {
+// 		color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+// 	}
+
+// 	return color;
+// }
